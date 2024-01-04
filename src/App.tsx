22b4cb2c88dddee225b2535a6402e9ec9ebc7c78
Khizar Khan <khezarhayat881@gmail.com>
2024-01-04 11:58:48 -0500

    Create project skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-[#fafafa]">
       <h1 className="text-3xl font-bold underline">Blog Service</h1>
     </div>
   );
