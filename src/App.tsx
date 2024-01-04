c7dee4bce59dfa0f82beb2bff8de27651fbda4b7
Khizar Khan <khezarhayat881@gmail.com>
2024-01-04 15:38:03 -0500

    add main blog page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Blog from "./components/Blog/Blog";
 
 function App() {
   return (
     <div className="bg-[#fafafa]">
       <Navbar />
+      <Blog />
     </div>
   );
 }
