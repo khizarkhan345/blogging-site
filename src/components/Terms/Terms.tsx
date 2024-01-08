b322d9babe4fe601c7344b88f11d67ae442bcc78
Khizar Khan <khezarhayat881@gmail.com>
2024-01-08 11:07:57 -0500

    create contact, about, cookies, privact and terms of service page

--- src/components/Terms/Terms.tsx
+++ src/components/Terms/Terms.tsx
@@ -1,7 +1,46 @@
 import React from "react";
 
 const Terms = () => {
-  return <h1>Terms</h1>;
+  return (
+    <div className="flex flex-col items-center w-[85%] sm:w-[80%] lg:w-[75%] mx-auto pb-[60px]">
+      <h1 className="text-[40px] text-center mb-[20px]">Terms of Service</h1>
+      <div>
+        <p className="text-[20px] mb-[30px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          voluptatibus exercitationem provident repellat quibusdam tempore
+          necessitatibus ipsam numquam blanditiis! Aut accusantium veritatis
+          excepturi, enim nisi maxime quisquam similique temporibus vero. Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Animi impedit
+          dolorem laborum natus quas in doloremque voluptatem officia
+          praesentium! Nam, fugit. Repellat voluptate quidem saepe modi
+          necessitatibus fuga amet assumenda?
+        </p>
+        <p className="text-[20px]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+          commodi eos labore. Et eligendi voluptatum, iste maiores quisquam
+          molestias aut ratione laudantium, minima nobis omnis optio repellat,
+          nihil quod expedita.
+        </p>
+        <p className="text-[20px] mb-[30px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi
+          voluptatibus exercitationem provident repellat quibusdam tempore
+          necessitatibus ipsam numquam blanditiis! Aut accusantium veritatis
+          excepturi, enim nisi maxime quisquam similique temporibus vero. Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Animi impedit
+          dolorem laborum natus quas in doloremque voluptatem officia
+          praesentium! Nam, fugit. Repellat voluptate quidem saepe modi
+          necessitatibus fuga amet assumenda?
+        </p>
+
+        <p className="text-[20px]">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+          commodi eos labore. Et eligendi voluptatum, iste maiores quisquam
+          molestias aut ratione laudantium, minima nobis omnis optio repellat,
+          nihil quod expedita.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Terms;
