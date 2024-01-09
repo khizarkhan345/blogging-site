df9aaaaf4da629edfb77e388073f95bd7b8622fe
Khizar Khan <khezarhayat881@gmail.com>
2024-01-09 18:36:49 -0500

    Connect with Contentful graphql API

--- src/components/BlogBox/BlogBox.tsx
+++ src/components/BlogBox/BlogBox.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import design from "../../images/design1.png";
 
-const BlogBox = () => {
+interface blogDataType {
+  title: string;
+  author: string;
+}
+const BlogBox = ({ title, author }: blogDataType) => {
   return (
     <div className="flex items-center justify-center relative w-[350px] h-[414px]  shadow-md offset-x-10 offset-y-5 bg-[#D3D3DB]">
       <img
@@ -10,11 +14,9 @@ const BlogBox = () => {
         className="w-[92%] max-w-full max-h-full"
       />
       <div className="w-[325px] h-[108px] bg-[#fff] absolute bottom-[70px]">
-        <p className="text-[24px] text-[#7C8093] pl-[3px]">
-          The Design Pos about quality design
-        </p>
+        <p className="text-[24px] text-[#7C8093] pl-[3px]">{title}</p>
         <div className="flex flex-row justify-between items-center">
-          <p className="text-[24px] text-[#4E9CA8] pl-[3px]">by khizar</p>
+          <p className="text-[24px] text-[#4E9CA8] pl-[3px]">by {author}</p>
           <p className="text-[20px] text-[#B3B3B3] pr-[5px]">
             -- January 3rd, 2024
           </p>
--- src/components/Blogs/Blogs.tsx
+++ src/components/Blogs/Blogs.tsx
@@ -1,25 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BlogBox from "../BlogBox/BlogBox";
 
+type YourElementType = string;
+
+// type YourElementType1 = {
+//   title:
+// }
+
+interface ContentValue {
+  value: string;
+}
+
+interface ContentArr {
+  content: ContentValue[];
+}
+interface ContentItem {
+  sys: {
+    id: string;
+  };
+  post: {
+    json: {
+      nodeType: string;
+      data: any;
+      content: any[]; // Adjust the type based on your actual content structure
+    };
+  };
+  title: string;
+  author: string;
+}
+
+interface obj1 {
+  title: string;
+  author: string;
+  post: YourElementType[];
+}
+
 const Blogs = () => {
+  const [blogPost, setBlogPost] = useState<obj1[]>([]);
+
+  const query = `
+  {
+  blogPost1Collection {
+    items {
+      sys {
+        id
+      }      
+      title
+      author
+      post {
+        json 
+      }
+    }
+  }
+ }
+  `;
+
+  const space_id = process.env.REACT_APP_SPACE_ID;
+  console.log(space_id);
+  const access_token = process.env.REACT_APP_ACCESS_TOKEN;
+
+  console.log(access_token);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://graphql.contentful.com/content/v1/spaces/${space_id}/`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            // Authenticate the request
+            Authorization: `Bearer ${access_token}`,
+          },
+          // send the GraphQL query
+          body: JSON.stringify({ query }),
+        }
+      );
+
+      let data = await response.json();
+
+      const items: ContentItem[] = data.data.blogPost1Collection.items;
+
+      items.forEach((item: ContentItem) => {
+        const obj: obj1 = {
+          title: "",
+          author: "",
+          post: [""],
+        };
+        const content = item.post.json.content;
+
+        obj.title = item.title;
+
+        obj.author = item.author;
+
+        content.forEach((element: ContentArr) => {
+          //console.log(element.content[0].value);
+
+          obj.post.push(element.content[0].value);
+        });
+
+        setBlogPost((prev) => {
+          return [...prev, obj];
+        });
+      });
+    })();
+  }, []);
+
+  //console.log(obj);
+  console.log(blogPost);
+
   return (
     <div className="pt-[30px] pb-[80px]">
       <h1 className="text-[40px] text-center mb-[60px]">Feature blogs</h1>
       <div className="grid grid-cols-1 justify-center justify-items-center gap-x-2 gap-y-[60px] sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-4">
-        <div>
-          <BlogBox />
-        </div>
-        <div>
-          <BlogBox />
-        </div>
-
-        <div>
-          <BlogBox />
-        </div>
-
-        <div>
-          <BlogBox />
-        </div>
+        {blogPost.map((blog: obj1) => (
+          <div>
+            <BlogBox title={blog.title} author={blog.author} />
+          </div>
+        ))}
       </div>
     </div>
   );
